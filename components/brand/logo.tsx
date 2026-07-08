4bb22f96fc56dd2d5220ee1ec2d179855469c611
Vedant Vyawahare <vedant@scoutflo.com>
2026-07-08 18:38:06 +0530

    feat: logo component + public brand assets

---
+++ components/brand/logo.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { site } from "@/content/site";
+
+export function Logo({ className = "", priority = false }: { className?: string; priority?: boolean }) {
+  return (
+    <Image
+      src="/brand/dustt-temple-logo.png"
+      alt={`${site.name} logo`}
+      width={636}
+      height={377}
+      priority={priority}
+      className={className}
+    />
+  );
+}
